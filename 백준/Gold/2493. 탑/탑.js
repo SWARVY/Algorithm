61afb67663cbc4cbaa586e91669c9fbe2eb18de2
Caffhheine <swarvy0826@naver.com>
2023-03-24 17:57:01 +0900

    [Gold V] Title: 탑, Time: 568 ms, Memory: 148176 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+// const str = "5\n6 9 5 7 4";
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+// const input = str.split("\n");
+const tower = input[1].split(" ").map(Number);
+const stack = [];
+const result = [];
+
+tower.forEach((value, idx) => {
+  const currTower = {
+    idx: idx + 1,
+    value,
+  };
+
+  if (!stack.length) {
+    stack.push(currTower);
+    result.push(0);
+    return;
+  }
+
+  if (stack[stack.length - 1].value < currTower.value) {
+    while (stack.length) {
+      if (stack[stack.length - 1].value >= currTower.value) break;
+      stack.pop();
+    }
+    result.push(!stack.length ? 0 : stack[stack.length - 1].idx);
+    stack.push(currTower);
+  } else {
+    result.push(!stack.length ? 0 : stack[stack.length - 1].idx);
+    stack.push(currTower);
+  }
+});
+console.log(result.join(" "));
