1a0f9a7241ffcb53d625642c20758444d28cc679
신현호 <swarvy0826@naver.com>
2024-04-02 17:48:04 +0900

    [Silver I] Title: 숨바꼭질, Time: 556 ms, Memory: 19952 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const [n, k] = input[0].split(' ').map(Number);
+
+const solve = () => {
+  const queue = [];
+  const visited = Array.from({ length: 100001 }, () => Infinity);
+
+  queue.push({ val: n, depth: 0 });
+  visited[n] = true;
+
+  while (queue.length) {
+    const { val, depth } = queue.shift();
+
+    if (val === k) {
+      console.log(depth);
+      return;
+    }
+
+    if (visited[val * 2] > depth + 1) {
+      queue.push({ val: val * 2, depth: depth + 1 });
+      visited[val * 2] = depth + 1;
+    }
+
+    if (visited[val + 1] > depth + 1) {
+      queue.push({ val: val + 1, depth: depth + 1 });
+      visited[val + 1] = depth + 1;
+    }
+
+    if (visited[val - 1] > depth + 1) {
+      queue.push({ val: val - 1, depth: depth + 1 });
+      visited[val - 1] = depth + 1;
+    }
+  }
+};
+
+solve();
