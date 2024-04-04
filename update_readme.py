9016627cbfe11dce25f6a0e423dcd31aa2de9a61
SWARVY <swarvy0826@naver.com>
2024-04-04 16:08:54 +0900

    fix : 링크 이미지 수정

--- update_readme.py
+++ update_readme.py
@@ -7,57 +7,40 @@ def extract_number(text):
     match = re.search(r'\d+', text)
     return int(match.group()) if match else 0
 
-def language_icon_link(filename, base_url):
-    """파일 확장자에 따라 해당 언어의 아이콘 링크를 생성합니다."""
-    extension_to_icon = {
-        "c": "c.svg",
-        "js": "javascript.svg",
-        # 필요한 경우 여기에 더 많은 언어와 해당 아이콘을 추가합니다.
-    }
-    extension = filename.split('.')[-1]
-    icon_filename = extension_to_icon.get(extension)
-    if icon_filename:
-        # 해당 언어의 아이콘 이미지 태그 반환
-        return f"<a href='{base_url}/{filename}' target='_blank'><img src='{base_url}/{icon_filename}' alt='{extension}' style='width: 20px; height: 20px;'/></a>"
-    return None
-
 def generate_readme_content(root_dir):
     content = []
     icons_base_url = "https://github.com/SWARVY/Algorithm/raw/main/icons"
 
-    # 사용한 언어 목록과 아이콘
-    content.append("<div align='center'>\n\n## 사용한 언어\n\n| 언어 | 아이콘 |\n|:----:|:----:|\n")
-    languages = {
-        "C": "c.svg",
-        "JavaScript": "javascript.svg",
-    }
-    for lang, icon in languages.items():
+    # 사용한 언어 아이콘만 포함하는 테이블
+    content.append("<div align='center'>\n\n## 사용한 언어\n\n")
+    languages = ["C", "JavaScript"]  # 사용한 언어 리스트
+    icons = ["c.svg", "javascript.svg"]  # 해당 언어의 아이콘 파일 이름
+
+    # 언어 아이콘 표시
+    for icon in icons:
         icon_url = f"{icons_base_url}/{icon}"
-        content.append(f"| {lang} | <img src='{icon_url}' alt='{lang}' style='width: 30px; height: 30px;'/> |\n")
+        content.append(f"<img src='{icon_url}' alt='icon' style='width: 30px; height: 30px; margin-right: 10px;' />\n")
+    
     content.append("\n</div>\n")
 
     # "백준" 섹션과 그 아래 내용
     content.append("<div align='center'>\n\n## 백준\n")
-    for platform in root_dir.iterdir():
-        if platform.is_dir() and platform.name == "백준":
-            levels = ["Bronze", "Silver", "Gold", "Platinum"]
-            for level_name in levels:
-                level_dir = platform / level_name
-                if level_dir.is_dir():
-                    # 등급 이미지 추가
-                    tier_image_url = f"{icons_base_url}/tier_{level_name.lower()}.png"
-                    content.append(f"\n### {level_name} <img src='{tier_image_url}' alt='{level_name}' style='width: 25px; height: 25px;'/>\n")
-                    content.append("| 문제 번호 | 문제 설명 | 풀이 |\n|:---------:|:---------:|:----:|\n")
-                    problem_dirs = sorted(level_dir.iterdir(), key=lambda x: extract_number(x.name))
-                    for problem_dir in problem_dirs:
-                        if problem_dir.is_dir():
-                            problem_name = problem_dir.name
-                            problem_url = f"https://github.com/SWARVY/Algorithm/tree/main/백준/{level_name}/{problem_name}"
-                            readme_url = f"{problem_url}/README.md"
-                            solution_files = [f for f in problem_dir.iterdir() if f.is_file() and f.name != "README.md"]
-                            # 언어 아이콘 링크를 생성합니다.
-                            solution_links = ' '.join([language_icon_link(f.name, problem_url) for f in solution_files if language_icon_link(f.name, icons_base_url)])
-                            content.append(f"| {problem_name} | [문제 설명]({readme_url}) | {solution_links} |\n")
+    platform_dir = root_dir / "백준"  # 백준 디렉토리 경로
+    levels = ["Bronze", "Silver", "Gold", "Platinum"]
+    for level_name in levels:
+        level_dir = platform_dir / level_name
+        if level_dir.exists():
+            tier_image_url = f"{icons_base_url}/tier_{level_name.lower()}.png"
+            content.append(f"\n### {level_name}\n<img src='{tier_image_url}' alt='{level_name}' style='width: 50px; height: auto;'/>\n")
+            content.append("| 문제 번호 | 풀이 |\n|:---------:|:----:|\n")
+            problem_dirs = sorted(level_dir.iterdir(), key=lambda x: extract_number(x.name))
+            for problem_dir in problem_dirs:
+                if problem_dir.is_dir():
+                    problem_name = problem_dir.name
+                    problem_url = f"https://github.com/SWARVY/Algorithm/tree/main/백준/{level_name}/{problem_name}"
+                    solution_files = [f for f in problem_dir.iterdir() if f.is_file() and f.name != "README.md"]
+                    solution_links = ' '.join([f"<a href='{problem_url}/{f.name}' target='_blank'><img src='{icons_base_url}/{f.suffix[1:]}.svg' alt='{f.suffix[1:]}' style='width: 20px; height: 20px;'/></a>" for f in solution_files])
+                    content.append(f"| {problem_name} | {solution_links} |\n")
     content.append("\n</div>\n")
 
     return "".join(content)
