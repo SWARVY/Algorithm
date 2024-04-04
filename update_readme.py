e9a86c1fc2abedfdf3e626e006f543a9cf2d0f56
SWARVY <swarvy0826@naver.com>
2024-04-04 19:30:43 +0900

    feat : 테이블 구성 변경

--- update_readme.py
+++ update_readme.py
@@ -7,53 +7,51 @@ def extract_number(text):
     match = re.search(r'\d+', text)
     return int(match.group()) if match else 0
 
-def generate_solution_links(solution_files, problem_url, icons_base_url):
-    """풀이 파일들에 대한 링크와 언어 아이콘을 생성합니다."""
+def generate_problems_section(platform_dir, levels, icons_base_url):
+    """문제 섹션을 생성합니다."""
+    content = ["<tr>"]
+    for level_name in levels:
+        level_dir = platform_dir / level_name
+        content.append(f"<td align='center' valign='top'><strong>{level_name}</strong><br/>")
+        if level_dir.exists():
+            problem_dirs = sorted(level_dir.iterdir(), key=lambda x: extract_number(x.stem))
+            for problem_dir in problem_dirs:
+                if problem_dir.is_dir():
+                    problem_name = ' '.join(problem_dir.name.split()[1:])  # 문제 이름
+                    problem_url = f"https://github.com/SWARVY/Algorithm/tree/main/백준/{level_name}/{problem_dir.name}"
+                    content.append(f"<a href='{problem_url}/README.md'>{problem_dir.name}</a><br/>")
+        else:
+            content.append("-")
+        content.append("</td>")
+    content.append("</tr>")
+    return "".join(content)
+
+def generate_solution_icons(solution_files, problem_url, icons_base_url):
+    """풀이 파일들에 대한 언어별 아이콘 링크를 생성합니다."""
     extension_to_icon = {
         ".c": "c.svg",
         ".js": "js.svg",  # 파일명 확인 필요
     }
-    solution_links = []
+    solution_icons = []
     for f in solution_files:
         icon_filename = extension_to_icon.get(f.suffix, None)  # 확장자에 해당하는 아이콘 파일 이름을 가져옵니다.
         if icon_filename:  # 해당하는 아이콘 파일 이름이 있는 경우에만 링크 생성
             icon_url = f"{icons_base_url}/{icon_filename}"
-            solution_link = f"<a href='{problem_url}/{f.name}' title='{f.suffix[1:]}'><img src='{icon_url}' alt='{f.suffix[1:]}' style='width: 20px; height: 20px;'/></a>"
-            solution_links.append(solution_link)
-    return ' '.join(solution_links)
-
-def generate_problems_table(platform_dir, level_name, icons_base_url):
-    """각 등급별 문제를 테이블로 생성합니다."""
-    level_dir = platform_dir / level_name
-    content = ["<table align='center'><tr><th>문제 이름</th><th>풀이 링크</th></tr>"]
-    if level_dir.exists():
-        problem_dirs = sorted(level_dir.iterdir(), key=lambda x: extract_number(x.stem))
-        for problem_dir in problem_dirs:
-            if problem_dir.is_dir():
-                problem_name = ' '.join(problem_dir.name.split()[1:])  # 문제 이름
-                problem_url = f"https://github.com/SWARVY/Algorithm/tree/main/백준/{level_name}/{problem_dir.name}"
-                solution_files = [f for f in problem_dir.iterdir() if f.is_file() and f.name != "README.md"]
-                solution_links = generate_solution_links(solution_files, problem_url, icons_base_url)
-                content.append(f"<tr><td>{problem_name}</td><td>{solution_links}</td></tr>")
-    else:
-        content.append("<tr><td colspan='2'>-</td></tr>")
-    content.append("</table>")
-    return "".join(content)
+            solution_icon = f"<a href='{problem_url}/{f.name}' title='{f.suffix[1:]}'><img src='{icon_url}' alt='{f.suffix[1:]}' style='width: 20px; height: 20px;'/></a>"
+            solution_icons.append(solution_icon)
+    return ' '.join(solution_icons)
 
 def generate_readme_content(root_dir):
-    content = ["<div align='center'>\n\n## 사용한 프로그래밍 언어\n\n"]
+    content = ["<table border='1' align='center'>"]
     icons_base_url = "https://github.com/SWARVY/Algorithm/raw/main/icons"
-
-    # "백준" 문제 섹션 처리
-    content.append("## 백준\n\n")
     platform_dir = root_dir / "백준"
-    if platform_dir.exists():
-        for level_name in ["Bronze", "Silver", "Gold", "Platinum", "Diamond", "Ruby"]:
-            content.append(f"### {level_name}\n")
-            content.append(generate_problems_table(platform_dir, level_name, icons_base_url))
-            content.append("\n")
-    content.append("\n</div>\n")
 
+    # 브론즈, 실버, 골드 문제 섹션
+    content.append(generate_problems_section(platform_dir, ["Bronze", "Silver", "Gold"], icons_base_url))
+    # 플래티넘, 다이아몬드, 루비 문제 섹션
+    content.append(generate_problems_section(platform_dir, ["Platinum", "Diamond", "Ruby"], icons_base_url))
+
+    content.append("</table>")
     return "".join(content)
 
 def update_readme(root_dir):
