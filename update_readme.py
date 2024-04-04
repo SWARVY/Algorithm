e9783ffc369909a068531be10696490508326011
SWARVY <swarvy0826@naver.com>
2024-04-04 19:44:27 +0900

    fix : 테이블 수정

--- update_readme.py
+++ update_readme.py
@@ -8,7 +8,6 @@ def main():
     content = ""
     content += HEADER
 
-    # Platinum, Diamond, Ruby 카테고리 추가
     categories = {
         'Bronze': {'nums': [], 'links': []},
         'Silver': {'nums': [], 'links': []},
@@ -18,72 +17,58 @@ def main():
         'Ruby': {'nums': [], 'links': []},
     }
 
+    # 문제 디렉토리 순회
     for root, dirs, files in os.walk("."):
         dirs.sort()
         if root == '.':
-            for dir in ('.git', '.github'):
-                try:
-                    dirs.remove(dir)
-                except ValueError:
-                    pass
+            dirs[:] = [d for d in dirs if d not in ('.git', '.github')]
             continue
 
         category = os.path.basename(root)
-
-        if category in ['images', 'icons']:  # icons 폴더도 무시
+        if category in ['images', 'icons']:
             continue
 
         directory = os.path.basename(os.path.dirname(root))
-
-        if directory == '.':
-            continue
-
         if directory not in categories:
             continue
 
         for file in files:
             file_ext = os.path.splitext(file)[1]
-            if file_ext in [".cc", ".c", ".java", ".js"]:  # '.js' 추가
+            if file_ext in [".cc", ".c", ".java", ".js"]:  # JavaScript 파일 포함
                 problem_num = category.split('_')[-1]
                 link = quote(os.path.join(root, file))
                 categories[directory]['nums'].append(problem_num)
                 categories[directory]['links'].append(link)
 
-    # Platinum, Diamond, Ruby 카운트 추가
-    bronze_count = len(categories['Bronze']['nums'])
-    silver_count = len(categories['Silver']['nums'])
-    gold_count = len(categories['Gold']['nums'])
-    platinum_count = len(categories['Platinum']['nums'])
-    diamond_count = len(categories['Diamond']['nums'])
-    ruby_count = len(categories['Ruby']['nums'])
-
-    # 총합과 최대 카운트 갱신
-    sum_count = bronze_count + silver_count + gold_count + platinum_count + diamond_count + ruby_count
-    max_count = max(bronze_count, silver_count, gold_count, platinum_count, diamond_count, ruby_count)
-
+    # README 내용 구성
     content += "## 📝 Solved Algorithm Problems by Backjoon Online Judge\n"
-    content += "\t- This repo is automatically managed using python & Github Action.\n"
-    content += "\t- This repo contains solved algorithm files written from {} sources.\n\n".format(sum_count)
-    
-    # 테이블 헤더에 Platinum, Diamond, Ruby 추가
-    content += "| Num | 🟤&nbsp;Bronze&nbsp;(Solved : {}) | ⚪&nbsp;Silver&nbsp;(Solved : {}) | 🟡&nbsp;Gold&nbsp;(Solved : {}) | 🟢&nbsp;Platinum&nbsp;(Solved : {}) | 🔵&nbsp;Diamond&nbsp;(Solved : {}) | 🔴&nbsp;Ruby&nbsp;(Solved : {}) |\n".format(
-        bronze_count, silver_count, gold_count, platinum_count, diamond_count, ruby_count
-    )
-    content += "| :-: | :-------------: | :------------: | :----------: | :----------: | :----------: | :----------: |\n"
+    content += "\t- This repo is automatically managed using python & Github Action.\n\n"
+    content += "### 🟤 Bronze ⚪ Silver 🟡 Gold\n"
+    content += generate_table(categories, ['Bronze', 'Silver', 'Gold'])
+    content += "\n### 🟢 Platinum 🔵 Diamond 🔴 Ruby\n"
+    content += generate_table(categories, ['Platinum', 'Diamond', 'Ruby'])
 
-    # 각 테이블 행을 생성
-    for i in range(max_count):
-        row_cells = [str(i + 1).zfill(2)]  # 테이블 행 시작에 번호 추가
-        for category in categories:
-            num = categories[category]['nums'][i] if i < len(categories[category]['nums']) else ''
-            link = categories[category]['links'][i] if i < len(categories[category]['links']) else ''
-            cell = "[{}]({})".format(num, link) if num and link else '-'
-            row_cells.append(cell)
-        content += "| " + " | ".join(row_cells) + " |\n"
-
+    # README 파일 작성
     with open("README.md", "w") as fd:
         fd.write(content)
 
+def generate_table(categories, levels):
+    max_count = max(len(categories[level]['nums']) for level in levels)
+    table_content = "| Num | " + " | ".join(levels) + " |\n| :-: | " + " | ".join([":-:" for _ in levels]) + " |\n"
+
+    for i in range(max_count):
+        row = f"| {str(i + 1).zfill(2)} "
+        for level in levels:
+            nums = categories[level]['nums']
+            links = categories[level]['links']
+            if i < len(nums):
+                row += f"| [{nums[i]}]({links[i]}) "
+            else:
+                row += "| - "
+        row += "|\n"
+        table_content += row
+
+    return table_content
 
 if __name__ == "__main__":
     main()
