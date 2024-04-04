1a8903171b0bcbf8efb5ffa778ba02aa4cbe09ce
SWARVY <swarvy0826@naver.com>
2024-04-04 16:24:14 +0900

    fix : 테이블 수정

--- update_readme.py
+++ update_readme.py
@@ -11,8 +11,7 @@ def generate_language_icons_table(icons_base_url):
     """사용한 프로그래밍 언어의 아이콘만 포함된 테이블을 생성합니다."""
     languages = {
         "C": "c.svg",
-        # "JavaScript": "javascript.svg", 파일 이름이나 경로에 문제가 없는지 확인하세요.
-        "JavaScript": "javascript.svg",  # 예시 경로, 실제 경로로 변경해야 할 수 있습니다.
+        "JavaScript": "javascript.svg",  # 파일명과 경로를 정확히 확인해주세요.
     }
     table_content = ["<table align='center'><tr>"]
     for lang, icon_filename in languages.items():
@@ -22,26 +21,24 @@ def generate_language_icons_table(icons_base_url):
     return "".join(table_content)
 
 def generate_readme_content(root_dir):
-    content = []
+    content = ["<div align='center'>\n\n## 사용한 프로그래밍 언어\n\n"]
     icons_base_url = "https://github.com/SWARVY/Algorithm/raw/main/icons"
 
     # 사용한 언어 아이콘 생성
-    content.append("<div align='center'>\n\n## 사용한 프로그래밍 언어\n\n")
     content.append(generate_language_icons_table(icons_base_url))
-    content.append("\n</div>\n")
+    content.append("\n\n")
 
     # "백준" 문제 섹션 처리
-    content.append("<div align='center'>\n\n## 백준\n")
+    content.append("## 백준\n\n")
     platform_dir = root_dir / "백준"
     if platform_dir.exists():
         levels = ["Bronze", "Silver", "Gold", "Platinum"]
         for level_name in levels:
             level_dir = platform_dir / level_name
             if level_dir.exists():
-                # 등급 이미지 추가
                 tier_image_url = f"{icons_base_url}/tier_{level_name.lower()}.png"
-                content.append(f"\n### <img src='{tier_image_url}' alt='{level_name}' style='vertical-align: middle; width: 50px; height: auto;'/> {level_name}\n")
-                content.append("<table align='center'><tr><th>문제 번호</th><th>풀이</th></tr>")
+                content.append(f"### <img src='{tier_image_url}' alt='{level_name}' style='vertical-align: middle; width: 40px; height: 40px;'/> {level_name}\n")
+                content.append("| 문제 번호 | 풀이 |\n|:---------:|:----:|\n")
                 problem_dirs = sorted(level_dir.iterdir(), key=lambda x: extract_number(x.stem))
                 for problem_dir in problem_dirs:
                     if problem_dir.is_dir():
@@ -49,8 +46,7 @@ def generate_readme_content(root_dir):
                         problem_url = f"https://github.com/SWARVY/Algorithm/tree/main/백준/{level_name}/{problem_dir.name}"
                         solution_files = [f for f in problem_dir.iterdir() if f.is_file() and f.name != "README.md"]
                         solution_links = ' '.join([f"<a href='{problem_url}/{f.name}' title='{f.suffix[1:]}'><img src='{icons_base_url}/{f.suffix[1:]}.svg' alt='{f.suffix[1:]}' style='width: 20px; height: 20px;'/></a>" for f in solution_files])
-                        content.append(f"<tr><td align='center'>{problem_name}</td><td align='center'>{solution_links}</td></tr>")
-                content.append("</table>\n")
+                        content.append(f"| [{problem_name}]({problem_url}/README.md) | {solution_links} |\n")
     content.append("\n</div>\n")
 
     return "".join(content)
