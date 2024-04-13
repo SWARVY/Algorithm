fef77b849d8882e94ce617d1727bde45c90d0211
신현호 <swarvy0826@naver.com>
2024-04-13 23:30:36 +0900

    [Bronze V] Title: 사칙연산, Time: 128 ms, Memory: 9608 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const [a, b] = input.shift().split(' ').map(Number);
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(parseInt(a / b));
+console.log(a % b);
