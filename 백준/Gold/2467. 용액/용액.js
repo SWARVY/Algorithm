331ddd1c9b16a84bfe5e77e74587f7b91aa824f4
신현호 <swarvy0826@naver.com>
2024-04-25 18:52:06 +0900

    [Gold V] Title: 용액, Time: 228 ms, Memory: 21592 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const n = +input.shift();
+const numbers = input[0].split(' ').map(Number);
+
+const solve = () => {
+  const curr = {
+    start: 0,
+    end: n - 1,
+  };
+  const answer = {
+    info: [null, null],
+    value: Infinity,
+  };
+
+  while (curr.start !== curr.end) {
+    if (answer.value > Math.abs(numbers[curr.start] + numbers[curr.end])) {
+      [answer.info[0], answer.info[1]] = [
+        numbers[curr.start],
+        numbers[curr.end],
+      ];
+      answer.value = Math.abs(numbers[curr.start] + numbers[curr.end]);
+    }
+
+    if (numbers[curr.start] + numbers[curr.end] < 0) {
+      curr.start += 1;
+    } else {
+      curr.end -= 1;
+    }
+  }
+
+  console.log(answer.info.join(' '));
+};
+
+solve();
